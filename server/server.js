556eb38aff6c6f7c94fe75981ff31caf69eb7fc6
Mukesh B <mbhalani3@gmail.com>
2017-05-12 13:38:37 -0400

    created basic express server and index.html

---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.use(express.static('./dist'));
+
+app.use('/', (req, res) => {
+  res.sendFile(path.resovle('client/index.html'));
+});
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`Express-Server listening on port: ${port}`);
+});
